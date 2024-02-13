c5495266b4225d0deb6bce51e7954fbd4fd6ba2f
DST-Gateway <lawarsenik@gmail.com>
2024-02-13 11:12:11 -0300

    Back in time

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,7 +7,7 @@ class Base:
     def __init__(self, id=None):
         """__init__ initialized constructor"""
         if id is None:
+            self.id = id
+        else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        else:
-            self.id = id
