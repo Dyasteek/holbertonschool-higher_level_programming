3fafdd5ae3e75bdac2d2b6bfce1c4ff7018fa127
DST-Gateway <lawarsenik@gmail.com>
2024-02-13 11:16:45 -0300

    Back in time

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 class Base:
-    """Base: Class define base"""
-    __nb_objects = 0
+	"""Base: Class define base"""
+	__nb_objects = 0
 
-    def __init__(self, id=None):
-        """__init__ initialized constructor"""
-        if id is None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+	def __init__(self, id=None):
+		"""__init__ initialized constructor"""
+		if id is None:
+			self.id = id
+		else:
+			Base.__nb_objects += 1
+			self.id = Base.__nb_objects
