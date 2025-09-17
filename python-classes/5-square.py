b43a7e419d7d4e80a1e7fecc234d1864243170e3
Dyasteek <lawarsenik@gmail.com>
2025-09-17 12:39:31 -0300

    Added stdout print with char #

---
+++ python-classes/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Private instance"""
+
+    def __init__(self, size=0):
+        """Atribute init
+
+        Args:
+            size (int): size of square
+        """
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area calculator"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Atribute init
+
+        Args:
+            size (int): size of square
+        """
+        if value != int(value):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        
+    def my_print(self):
+        """Print with #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+        return
