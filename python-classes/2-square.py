c01b91141b48c1b30a42b05238c07e701bb98644
Dyasteek <lawarsenik@gmail.com>
2025-09-17 10:19:09 -0300

    Adding conditionals to atribute

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Private instance"""
+
+    def __init__(self, size=0):
+        """Atribute init
+
+        Args:
+            size (int): size of square
+        """
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
