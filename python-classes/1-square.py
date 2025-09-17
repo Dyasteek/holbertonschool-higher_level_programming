557e0113c88be3455cf69da77340ed0e91312fa2
Dyasteek <lawarsenik@gmail.com>
2025-09-17 10:13:08 -0300

    Square class with size atribute

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """Creating class square"""
 
+
 class Square:
     """Class square"""
+
     def __init__(self, size=0):
         """Defined size
+
         Args:
             size (int): size of square
         """
