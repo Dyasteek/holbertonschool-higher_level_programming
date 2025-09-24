eb92120e4607c3d14e65e68b5b2a18822dc31cc5
Dyasteek <lawarsenik@gmail.com>
2025-09-23 23:27:54 -0300

    Public instance method: def area(self): that raises an Exception with a message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""An empty class"""
+
+
+class BaseGeometry():
+    """An empty class"""
+    def area(self):
+        """An empty method"""
+        raise Exception("area() is not implemented")
