5e981c4c51b38e9ff197babe2a091d11307e8c9d
Dyasteek <lawarsenik@gmail.com>
2025-09-24 10:51:43 -0300

    Print() should print, and str() should return, the following rectangle description: [Rectangle] <width>/<height>

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class that defines a rectangle"""
+    def __init__(self, width, height):
+        """A method that initializes the rectangle"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    
+    def area(self):
+        """A method that returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """A method that returns the string representation of the rectangle"""
+        return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
