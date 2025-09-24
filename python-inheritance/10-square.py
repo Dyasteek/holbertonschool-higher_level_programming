5903c0bb05369013187d9dd4af70eead2a476e64
Dyasteek <lawarsenik@gmail.com>
2025-09-24 11:07:13 -0300

    Class Rectangle that inherits from BaseGeometry (7-base_geometry.py). (task based on 8-rectangle.py)

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A class that defines a square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class that defines a square"""
+    def __init__(self, size):
+        """A method that initializes the square"""
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+    
+    def area(self):
+        """A method that returns the area of the square"""
+        return self.__size ** 2
