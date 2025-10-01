03f4e7b4d5fbf4acb75d3475c6c464ede45cd489
Dyasteek <lawarsenik@gmail.com>
2025-10-01 13:40:22 -0300

    pycodestyle validation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A function that reads a text file and prints it to stdout"""
 
+
 def read_file(filename=""):
     """printing the content of a file"""
     with open(filename, encoding="utf-8") as f:
