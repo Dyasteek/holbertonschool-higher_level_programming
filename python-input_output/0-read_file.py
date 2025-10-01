701fca860b7b0279700273982460ecec23dd4fd0
Dyasteek <lawarsenik@gmail.com>
2025-10-01 13:35:51 -0300

    Changed the indent format

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 """A function that reads a text file and prints it to stdout"""
 
 def read_file(filename=""):
-	"""printing the content of a file"""
-	with open(filename, encoding="utf-8") as f:
-		print(f.read(), end="")
+    """printing the content of a file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
