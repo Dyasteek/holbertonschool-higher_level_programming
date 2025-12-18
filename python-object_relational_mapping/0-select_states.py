4e90ce6b54cf8c74b1e1300a3c6ad90a7b6ee946
Dyasteek <lawarsenik@gmail.com>
2025-12-18 01:33:44 -0300

    Added module comments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
 
 import MySQLdb
 import sys
 
 
 def main():
+    """
+    Connects to the database and lists all states.
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -32,3 +38,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
