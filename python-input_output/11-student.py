972cedb00cbbf0ce218b54e75c7831e2f79bebdf
Dyasteek <lawarsenik@gmail.com>
2025-12-18 01:22:36 -0300

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for Student class"""
+
+
+class Student:
+    """Class that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance
+        If attrs is a list of strings, only those attributes are retrieved
+        Otherwise, all attributes are retrieved"""
+        result = {}
+        for key, value in self.__dict__.items():
+            if isinstance(value, (list, dict, str, int, bool)):
+                if attrs is None or key in attrs:
+                    result[key] = value
+        return result
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
